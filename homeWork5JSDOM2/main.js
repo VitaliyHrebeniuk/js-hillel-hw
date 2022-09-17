1cbb345c7ad4f247a9190e64edaa78e6d0746917
Vitalii <vitaliy.hrebeniuk@gmail.com>
2022-09-17 22:08:14 +0300

    Пятое ДЗ!исправленное

--- homeWork5JSDOM2/main.js
+++ homeWork5JSDOM2/main.js
@@ -1,14 +1,9 @@
 let demo = document.getElementById('demo');
 let button = document.getElementById('choiceColor');
-let figureArray = {
-    cube:'cube',
-    rectangle:'rectangle',
-    circle: 'circle'
-};
 
 let figure = () =>{
     let howToSelect = document.getElementById('figure').value;
-    demo.setAttribute('id',figureArray[howToSelect])
+    demo.setAttribute('id', howToSelect)
 }
 
 button.addEventListener('click',()=>{
