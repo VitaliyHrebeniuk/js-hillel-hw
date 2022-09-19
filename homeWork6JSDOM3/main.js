23d2218c5f207eef050e221b3187078336246c94
vitaliihrebeniuk <vitaliy.hrebeniuk@gmail.com>
2022-09-19 14:44:06 +0300

    DOM3 JS 2.0

--- homeWork6JSDOM3/main.js
+++ homeWork6JSDOM3/main.js
@@ -1,16 +1,19 @@
 let input = document.querySelector('#to-do');
 let button = document.querySelector('#addButton')
 let ul = document.querySelector('ul')
+let li = '';
 
 button.addEventListener('click',()=>{
-    let li = ul.innerHTML;
     li += `<li class="yellow"> ${input.value} <button>Delete</button></li>`
     ul.innerHTML = li;
+    input.value = '';
 })
 
 ul.addEventListener('click', (event) =>{
-    event.target.classList.toggle('green');
-    if(event.target.tagName == 'BUTTON'){
+    if(event.target.tagName === 'LI'){
+        event.target.classList.toggle('green');
+    }
+    if(event.target.tagName === 'BUTTON'){
         event.target.parentElement.remove();
     }
 })
