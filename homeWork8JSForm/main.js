0b1a0d922bd68b3745ef82e2150ad2af17cea592
vitaliihrebeniuk <vitaliy.hrebeniuk@gmail.com>
2022-09-29 12:14:19 +0300

    пока не сделал сабмит

---
+++ homeWork8JSForm/main.js
@@ -0,0 +1,19 @@
+const login = document.querySelector('.login');
+const pswd = document.querySelector('.pswd');
+const submit = document.querySelector('.submit');
+
+login.addEventListener('change', () =>{
+    if (/[0-9a-zA-z]{2,15}/.test(login.value)===false){
+        login.classList.add('red')
+    }else{
+        login.classList.remove('red')
+    }
+});
+
+pswd.addEventListener('change', () =>{
+    if (/^[a-z0-9#$]{2,15}$/.test(pswd.value)===false){
+        pswd.classList.add('red')
+    }else{
+        pswd.classList.remove('red')
+    }
+});
